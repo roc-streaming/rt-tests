c20bc67874aeb781398c8d09f315294c2e6fe9d9
Victor Gaydov <victor@enise.org>
2024-01-24 14:21:52 +0400

    Don't try to update dependencies

--- cmake/download_googletest.cmake
+++ cmake/download_googletest.cmake
@@ -6,6 +6,7 @@ ExternalProject_Add(googletest_lib
   GIT_SHALLOW       ON
   SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
   BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+  UPDATE_COMMAND    ""
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
   LOG_DOWNLOAD      ON
